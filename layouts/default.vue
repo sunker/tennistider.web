8f49368844c06c4fe1ba69c7101974f532f4bae7
Erik Sundell <erik.sundell87@gmail.com>
2017-10-12 19:21:49 +0200

    Intial commit

---
+++ layouts/default.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <nuxt/>
+  </div>
+</template>
+
+<style>
+html {
+  font-family: "Roboto", sans-serif;
+  font-weight: 300;
+  background: #76b852; /* fallback for old browsers */
+  background: -webkit-linear-gradient(right, #76b852, #8DC26F);
+  background: -moz-linear-gradient(right, #76b852, #8DC26F);
+  background: -o-linear-gradient(right, #76b852, #8DC26F);
+  background: linear-gradient(to left, #76b852, #8DC26F);
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+.container {
+  width: 95%;
+  max-width: 400px;
+  position: relative;
+  z-index: 1;
+  background: #FFFFFF;
+  margin-top: 10px;
+  margin-left: auto ;
+  margin-right: auto ;
+  box-shadow: 0 0 20px 0 rgba(0, 0, 0, 0.2), 0 5px 5px 0 rgba(0, 0, 0, 0.24);
+}
+
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,79 @@
+<template>
+  <section class="container">
+    <div>
+      <logo/>
+      <h1 class="title">
+        tennistider
+      </h1>
+      <hr />
+      <ul class="menu">
+        <li>
+          Hitta lediga tider
+        </li>
+        <li>
+          Skapa bevakning
+        </li>
+      </ul>
+    </div>
+  </section>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+
+export default {
+  components: {
+    Logo
+  }
+}
+</script>
+
+<style>
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+h1 {
+  margin: 0;
+}
+
+logo {
+  margin: 20px 20px;
+}
+
+.menu {
+    list-style-type: none;
+}
+
+.menu ul {
+    margin-left: 0;
+}
+
+.menu li {
+    border-bottom: 1px solid lightgray !important;
+}
+
+.title {
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
+  display: block;
+  font-weight: 300;
+  font-size: 60px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
